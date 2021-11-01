e2f6e40acc3afe6f5a1314830856e0186f266e10
Ramit Amitabh <ramit.amitabh17@gmail.com>
2021-11-01 19:15:36 +0530

    App v2 test changes

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,7 @@ def test_hello():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data == b'Hello Stranger!!!'
+    assert response.data == b'Hello Stranger V2!!!'
 
 def test_hello1():
     response = app.test_client().get('/helloworld')
